5669dbaeddb80cf8dc98453aa12052700ca1812a
Hugo Gonzalez <hugo.gonzalez@upslp.edu.mx>
2025-07-05 13:19:05 -0600

    Feature for insert screenshots embedded into the final html added

--- ctidiagram.py
+++ ctidiagram.py
@@ -3,6 +3,9 @@
 # Copyright (C) 2023 - Hugo Gonzalez (@hugo_glez)
 # This file is part of CTIdiagram
 
+
+__version__ = '2.0'
+
 """
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
@@ -34,7 +37,11 @@ import yaml
 import sys
 from datetime import datetime
 import argparse 
+import base64
+import uuid
 
+
+encoded_resources = {}
 
 
 header = """
@@ -72,15 +79,132 @@ tr.iocs {
   font-size:14px;
 }
 
+
+#myImg {
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+#myImg:hover {opacity: 0.7;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (image) */
+.modal-content {
+  margin: auto;
+  display: block;
+  width: 90%;
+
+}
+
+/* Caption of Modal Image */
+#caption {
+  margin: auto;
+  display: block;
+  width: 90%;
+  max-width: 900px;
+  text-align: center;
+  color: #ccc;
+  padding: 10px 0;
+  height: 150px;
+}
+
+/* Add Animation */
+.modal-content, #caption {  
+  -webkit-animation-name: zoom;
+  -webkit-animation-duration: 0.6s;
+  animation-name: zoom;
+  animation-duration: 0.6s;
+}
+
+@-webkit-keyframes zoom {
+  from {-webkit-transform:scale(0)} 
+  to {-webkit-transform:scale(1)}
+}
+
+@keyframes zoom {
+  from {transform:scale(0)} 
+  to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+  position: absolute;
+  top: 15px;
+  right: 35px;
+  color: #f1f1f1;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+
+
+
 </style>
 </head>
 <body>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+  <span class="close">&times;</span>
+  <img class="modal-content" id="img01">
+  <div id="caption"></div>
+</div>
+
 <center>
 """
 
 
 footer = """
 </center>
+
+<script>
+// Get the modal
+
+function ShowModal(param) {
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg");
+    var modalImg = document.getElementById("img01");
+
+    modal.style.display = "block";
+    eval("modalImg.src = "+ param );
+
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+</script>
+
 </body>
 </html>
 """
@@ -100,10 +224,46 @@ def logo():
     """
     )
 
+def print_resources():
+    
+    print ("<script>")
+    for k,v in encoded_resources.items():
+        print ("const "+k+" = \"",v+"\";")
+    print ("</script>")
+
+
+def convert_image_2_base64(filep, token):
+    #print(filep)
+    with open(filep,'rb') as f:
+        rawbytes = f.read()
+    encoded = base64.b64encode(rawbytes)
+    encoded_string = encoded.decode('utf-8')
+    encoded_resources[token]="data:image/png;base64," + encoded_string
+    #encoded_resources[token]=filep
+    
+
+
+def print_icons(resource_folder, data):
+    if type(data) == list:
+        for ico in data:
+            if type(ico) == dict:
+                nico = list(ico.keys())[0]
+                token = "T"+str(uuid.uuid4())[:6]
+                print ("<a href=\"javascript:ShowModal('"+token+"')\"><img src='"+resource_folder+nico+".png'></a>")
+                convert_image_2_base64(ico[nico][0],token)
+                
+            else:
+                print ("<img src='"+resource_folder+ico+".png'>")
+    else:
+       if type(data) == dict:
+           ndata = list(data.keys())[0]
+           print ("<img src='"+resource_folder+ndata+".png'>")
+       else:
+           print ("<a href='script'><img src='"+resource_folder+data+".png'></a>")
 
 def main():
 
-    parser = argparse.ArgumentParser(description ="Convert YAML file to attack flow diagram")
+    parser = argparse.ArgumentParser(description ="Convert YAML file to CTI diagram")
     parser.add_argument("yamlfile", help="YAML file to convert")
     parser.add_argument("-o","--output", help="File to save the output",  type=argparse.FileType('w'))
     parser.add_argument("-r","--resources", help="Directory to get images, default is imgs/", default = "imgs/")
@@ -135,10 +295,12 @@ def main():
     
 
 
-    diagrama = doc['diagram']
+    diagrama = doc['diagrama']
+    #print (type(diagrama))
+    #print (diagrama)
 
 
-    mdate = doc.get("date", " ")
+    mdate = doc.get("fecha", " ")
     mtitle = doc.get("title", "Here is your title!")
 
 
@@ -170,19 +332,11 @@ def main():
     for a in diagrama[:-1]:
         print ("<td>")
         ticon = a.get("icon", "default")
-        if type(ticon) == list:
-            for ico in ticon:
-                print ("<img src='"+resource_folder+ico+".png'>")
-        else:
-            print ("<img src='"+resource_folder+ticon+".png'>")
+        print_icons(resource_folder, ticon)
         print ("</td><td><img src='"+resource_folder+"flecha.png'> </td>")
     print ("<td>")
     ticon = diagrama[-1].get("icon", "default")
-    if type(ticon) == list:
-        for ico in ticon:
-            print ("<img src='"+resource_folder+ico+".png'>")
-    else:
-        print ("<img src='"+resource_folder+ticon+".png'>")
+    print_icons(resource_folder, ticon)
     print ('</td>')
     print ('</tr>')
 
@@ -253,6 +407,7 @@ def main():
 
     print('<br><br>Generated on '+datetime.now().strftime("%Y-%m-%d, %H:%M") + ' by CTIdiagrams')
 
+    print_resources()
     print (footer)
 
     if args.output:
@@ -264,6 +419,11 @@ def main():
 if __name__ == "__main__":
     main()
     
+
+
+
+
+
 
 
 
